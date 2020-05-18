44a0708c713b6f7601c0b8f69ce6be13dcaefa0e
Hakizu <hakisuj@gmail.com>
2020-05-18 10:49:55 +0200

    chaging var names & removing unused parts - form.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,26 @@
 import React, {useState, useEffect} from 'react'
 import Filter from "./Components/Filter"
-import Form from "./Components/Form"
 import Result from "./Components/Result"
 import Axios from 'axios'
 
 const App = () => {
-    const [persons, setPersons] = useState([])
-    const [newNumber, setNewNumber] = useState('')
-    const [newName, setNewName] = useState('')
+    const [country, setCountries] = useState([])
     const [newSearch, setNewSearch] = useState("")
 
     const hook = () => {
         Axios
             .get("https://restcountries.eu/rest/v2/all")
             .then(response => {
-                setPersons(response.data)
+                setCountries(response.data)
             })
     }
     useEffect(hook, [])
-
-    const newPerson = (event) =>{
-        event.preventDefault()
-        const personObject = {
-            name : newName,
-            number : newNumber
-        }
-        const exists = persons.map(p => p.name.includes(newName))
-
-        exists.includes(true)
-        ? alert(`${newName} is already added to the phonebook`)
-        : setPersons(persons.concat(personObject))
-
-        setNewName("")
-        setNewNumber("") 
-    }  
 
     return (
         <div>
             <h2>List of countries</h2>
             <Filter setNewSearch={setNewSearch} newSearch={newSearch}/>
-            {/*<h2>Add new Contact</h2>
-            <Form newPerson={newPerson} newName={newName} setNewName={setNewName} 
-                newNumber={newNumber} setNewNumber={setNewNumber} /> */}
-            <Result newSearch={newSearch} persons={persons} />
+            <Result newSearch={newSearch} country={country} />
         </div>
     )
 }
--- src/Components/Result.js
+++ src/Components/Result.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Result = ({newSearch, persons}) => {
+const Result = ({newSearch, country}) => {
     
     const result = !newSearch
-    ? persons
-    : persons.filter(p =>
-        p.name.toLowerCase().includes(newSearch.toLowerCase()))
+    ? country
+    : country.filter(c =>
+        c.name.toLowerCase().includes(newSearch.toLowerCase()))
 
     if (result.length === 1){
         return (
@@ -28,8 +28,8 @@ const Result = ({newSearch, persons}) => {
         return (
             <div>
             <h2>Countries</h2>
-            {result.map(persons => 
-                <li key={persons.name}> {persons.name} {persons.number} </li>)}
+            {result.map(c => 
+                <li key={c.name}> {c.name} {c.number} </li>)}
                 </div>
             )
     }
